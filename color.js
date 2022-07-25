25f77efa2187ea012be7bf940691c558f17d4545
Belial-FrontEnd-Dev <102366429+Belial-FrontEnd-Dev@users.noreply.github.com>
2022-07-25 16:11:03 +0200

    Delete color.js

--- color.js
+++
@@ -1,9 +0,0 @@
-const menu = document.getElementById("mobile-menu");
-const menuLinks = document.querySelector(".navbar__menu");
-
-menu.addEventListener("click", () => {
-    menu.classList.toggle("is-active");
-    menuLinks.classList.toggle("active");
-});
-
-const navLinks = document.querySelectorAll("navbar__links");
